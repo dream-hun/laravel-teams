6c3fa95de48bf5bfee6042c34acf9faba33139ff
Alex Garrett-Smith <alex@codecourse.com>
2024-11-21 10:44:30 +0000

    Episode 05

--- tests/Feature/Observers/UserObserverTest.php
+++ tests/Feature/Observers/UserObserverTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Team;
 use App\Models\User;
+use function Pest\Laravel\assertDatabaseEmpty;
 
 it('creates a personal team when a user is created', function () {
     $user = User::factory()->create([
@@ -10,4 +12,14 @@ it('creates a personal team when a user is created', function () {
     expect($user->teams)
         ->toHaveCount(1)
         ->first()->name->toBe($user->name);
+});
+
+it('removes all team attachments when deleted', function () {
+    $user = User::factory()
+        ->has(Team::factory()->times(2))
+        ->createQuietly();
+
+    $user->delete();
+
+    assertDatabaseEmpty('team_user');
 });
