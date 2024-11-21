8d99ffe937c3246f140a60a7770d6f2fe3442780
Alex Garrett-Smith <alex@codecourse.com>
2024-11-21 11:58:50 +0000

    Episode 09

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SetCurrentTeamRequest;
 use App\Models\Team;
-use Illuminate\Http\Request;
 
 class TeamController extends Controller
 {
-    public function setCurrent(Request $request, Team $team)
+    public function setCurrent(SetCurrentTeamRequest $request, Team $team)
     {
-        // authorize
-
         $request->user()->currentTeam()->associate($team)->save();
 
         return back();
