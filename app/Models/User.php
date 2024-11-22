de07dd9cb0c365b8050abc35cf19a8915de7b5f3
Alex Garrett-Smith <alex@codecourse.com>
2024-11-22 09:02:00 +0000

    Episode 17

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,11 @@ class User extends Authenticatable
         ];
     }
 
+    public function profilePhotoUrl()
+    {
+        return 'https://gravatar.com/avatar/' . md5($this->email) . '?s=100';
+    }
+
     public function teams()
     {
         return $this->belongsToMany(Team::class);
